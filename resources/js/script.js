4ff3391c96d6d4ff1854344d88f640a503030aa2
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-07-31 22:57:28 -0700

    added animation on scroll effects

--- resources/js/script.js
+++ resources/js/script.js
@@ -16,11 +16,31 @@ $('.js--scroll-to-start').click(function(){
   $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000)
 });
 
-/*  var waypoints = $('#handler-first').waypoint(function(direction) {
-    notify(this.element.id + ' hit 25% from top of window')
-  }, {
-    offset: '25%'
-  }) */
+/*animation on scroll*/
+
+$('.js--wp-1').waypoint(function(direction){
+  $('.js--wp-1').addClass('animated fadeIn');
+},{
+    offset: '50%'
+});
+
+$('.js--wp-2').waypoint(function(direction){
+  $('.js--wp-2').addClass('animated fadeInUpBig')
+},{
+    offset: '50%'
+});
+
+$('.js--wp-3').waypoint(function(direction){
+  $('.js--wp-3').addClass('animated fadeIn')
+},{
+    offset: '50%'
+});
+
+$('.js--wp-4').waypoint(function(direction){
+  $('.js--wp-4').addClass('animated pulse')
+},{
+    offset: '50%'
+});
 
 
 });
